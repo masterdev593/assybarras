3489511f2550a611afbfd78b0ba43cdc8d6edb1f
Vladimir Rodríguez <fizzvr@gmail.com>
2019-08-16 11:59:08 -0500

    BETA ini

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eius nesciunt dolores officiis corrupti, quis similique perspiciatis ut modi exercitationem praesentium quam dolore. Dolores repudiandae maxime autem recusandae, qui sint. </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
