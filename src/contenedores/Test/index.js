68bc6837977e6864e27f3c05f905802149b943c8
Vladimir Rodríguez <fizzvr@gmail.com>
2019-08-20 16:09:48 -0500

    Test Ruta

---
+++ src/contenedores/Test/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TestPagina = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Totam temporibus nam alias, soluta recusandae itaque dolorum eius repellat laboriosam possimus ipsum magnam vel accusantium blanditiis ad voluptatum necessitatibus eveniet impedit.
+      Soluta ratione provident accusamus! Sed iure optio expedita eveniet, assumenda natus atque repellendus sit maxime quidem asperiores aliquam, architecto molestias harum, officiis vero at quibusdam voluptatum impedit mollitia sunt odio.
+      Itaque beatae nulla, aut dicta dolore quod quos quas labore necessitatibus excepturi quibusdam voluptatum nihil dolorum natus in cumque! Aperiam mollitia laborum explicabo aliquid quas accusantium sunt quam temporibus consequuntur!</p>
+    </div>
+  );
+};
+
+export default TestPagina;
