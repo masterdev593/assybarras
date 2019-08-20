cf3a6380d235d4f09a88fac82c7e8bcb7bba3088
Vladimir Rodríguez <fizzvr@gmail.com>
2019-08-20 16:09:19 -0500

    Main ruta

---
+++ src/contenedores/Rutas/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import TestPagina from '../Test';
+import ProgramaPagina from '../ProgramaPagina';
+
+const RutasAssy = () => {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route component={ProgramaPagina} exact={true} path="/" />
+          <Route component={TestPagina} exact={true} path="/test" />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default RutasAssy;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/styles';
-import App from './App';
+import App from './contenedores/Rutas';
 import theme from './tema';
 import * as serviceWorker from './serviceWorker';
 
