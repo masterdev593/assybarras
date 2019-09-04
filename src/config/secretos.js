1137279581deb6cdeb291eb55009bc79bff8b305
Vladimir Rodríguez <fizzvr@gmail.com>
2019-09-03 19:09:22 -0500

    secrets from env file

---
+++ src/config/secretos.js
@@ -0,0 +1,10 @@
+module.exports = {
+  firebase: {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATA_BASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER,
+    appId: process.env.REACT_APP_APP_ID
+  }
+};
