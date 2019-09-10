87bd9fcac76b8924ed73ed530974d1c285a423c5
Vladimir Rodríguez <fizzvr@gmail.com>
2019-09-10 17:33:53 -0500

    rework action

--- src/base/acciones/mensaje.Acciones.js
+++ src/base/acciones/mensaje.Acciones.js
@@ -1,13 +1,15 @@
 import { MENSAJE_ENVIA } from './constantes';
 
-const sendNotification = (payload) => ({
-  type: MENSAJE_ENVIA,
-  payload
-})
-
-export const _cmdaddTodo = parte => (dispatch, getState, getFirebase) => {
-  const firebase = getFirebase();
-  firebase.push('cat_partes', parte).then(() => {
-    dispatch(sendNotification('Todo Added'));
-  });
+export const _cmdaddTodo = parte => {
+  return (dispatch, getState, getFirebase) => {
+    const firebase = getFirebase();
+    firebase
+      .push('cat_partes', parte)
+      .then(() => {
+        dispatch({
+          type: MENSAJE_ENVIA,
+          payload: 'OK'
+        });
+    });
+  };
 };
