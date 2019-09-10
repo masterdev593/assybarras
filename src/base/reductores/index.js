c2e21780e267c63d00e7b5d90e3d8d0072222f37
Vladimir Rodríguez <fizzvr@gmail.com>
2019-09-10 18:31:51 -0500

    add alerta to redux

--- src/base/reductores/index.js
+++ src/base/reductores/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase';
+import alertaReductor from './alerta.Reductor';
 
 export default combineReducers({
-  firebase: firebaseReducer
+  firebase: firebaseReducer,
+  alerta: alertaReductor
 });
