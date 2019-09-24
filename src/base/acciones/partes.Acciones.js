2acafda2aa3e964fdb590c7bdd94238e87aa6013
Vladimir Rodríguez <fizzvr@gmail.com>
2019-09-24 17:25:16 -0500

    New function

--- src/base/acciones/partes.Acciones.js
+++ src/base/acciones/partes.Acciones.js
@@ -1,10 +1,10 @@
-import { ALERTA_SATISFACTORIA } from './constantes';
+import { ALERTA_SATISFACTORIA, FETCH_TODOS } from './constantes';
 
 export const _cmdaddTodo = parte => {
   return (dispatch, getState, getFirebase) => {
     const firebase = getFirebase();
     firebase
-      .push('cat_partes', parte)
+      .push('partez', parte)
       .then(() => {
         dispatch({
           type: ALERTA_SATISFACTORIA,
@@ -13,3 +13,33 @@ export const _cmdaddTodo = parte => {
     });
   };
 };
+
+export const _cmdgetPartes = () => {
+  return (dispatch, getState, getFirebase) => {
+    const firebase = getFirebase();
+    firebase
+      .on('value', snapshot => {
+        dispatch({
+          type: FETCH_TODOS,
+          payload: snapshot.val()
+        });
+    });
+  };
+};
+/*
+export const addToDo = newToDo => async dispatch => {
+  todosRef.push().set(newToDo);
+};
+
+export const completeToDo = completeToDo => async dispatch => {
+  todosRef.child(completeToDo).remove();
+};
+
+export const fetchToDos = () => async dispatch => {
+  todosRef.on("value", snapshot => {
+    dispatch({
+      type: FETCH_TODOS,
+      payload: snapshot.val()
+    });
+  });
+}; */
