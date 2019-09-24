da2ddaf62165d98fed6330a508bd07e08bf259fc
Vladimir Rodríguez <fizzvr@gmail.com>
2019-09-24 17:26:39 -0500

    Rework firebase config

--- src/base/configurarBase.dev.js
+++ src/base/configurarBase.dev.js
@@ -1,25 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import { getFirebase } from 'react-redux-firebase';
 import rootReducer from './reductores';
-import firebase from 'firebase/app';
-import 'firebase/database';
-import secretos from '../config/secretos';
 
 const configurarBase = preloadedState => {
-
-  var firebaseConfig = {
-    apiKey: secretos.firebase.apiKey,
-    authDomain: secretos.firebase.authDomain,
-    databaseURL: secretos.firebase.databaseURL,
-    projectId: secretos.firebase.projectId,
-    storageBucket: '',
-    messagingSenderId: secretos.firebase.messagingSenderId,
-    appId: secretos.firebase.appId
-  };
-
-  firebase.initializeApp(firebaseConfig);
 
   const middleware = [thunk.withExtraArgument(getFirebase), logger];
 
@@ -30,11 +15,7 @@ const configurarBase = preloadedState => {
     rootReducer,
     preloadedState,
     composeEnhancers(
-      applyMiddleware(...middleware),
-      reactReduxFirebase(firebase, {
-        userProfile: 'users',
-        enableLogging: false
-      })
+      applyMiddleware(...middleware)
     )
   );
 
--- src/index.js
+++ src/index.js
@@ -7,16 +7,42 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/styles';
 import App from './contenedores/Rutas';
 import theme from './tema';
+import secretos from './config/secretos';
 import * as serviceWorker from './serviceWorker';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
 const store = configurarBase();
 
+var firebaseConfig = {
+  apiKey: secretos.firebase.apiKey,
+  authDomain: secretos.firebase.authDomain,
+  databaseURL: secretos.firebase.databaseURL,
+  projectId: secretos.firebase.projectId,
+  storageBucket: '',
+  messagingSenderId: secretos.firebase.messagingSenderId,
+  appId: secretos.firebase.appId
+};
+
+const rrfConfig = { userProfile: 'users' };
+
+firebase.initializeApp(firebaseConfig);
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch
+};
+
 ReactDOM.render(
   <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('assybarras')
 );
