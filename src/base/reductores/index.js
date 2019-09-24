dd2a9a908e845881b6b422b12df17b2396df5e7a
Vladimir Rodríguez <fizzvr@gmail.com>
2019-09-24 17:24:55 -0500

    Combine partes reductor

--- src/base/reductores/index.js
+++ src/base/reductores/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase';
 import alertaReductor from './alerta.Reductor';
+import partesReductor from './partes.Reductor';
 
 export default combineReducers({
   firebase: firebaseReducer,
-  alerta: alertaReductor
+  alerta: alertaReductor,
+  partes: partesReductor
 });
