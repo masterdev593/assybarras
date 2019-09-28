cd1fe2872b30266049f4e055daf36141c0236d27
Vladimir Rodríguez <fizzvr@gmail.com>
2019-09-27 19:27:04 -0500

    Setup redux

---
+++ src/componentes/Zinventario/index.js
@@ -0,0 +1,31 @@
+import { bindActionCreators, compose } from 'redux';
+import { connect } from 'react-redux';
+import Inventario from './inventarioMain';
+import { _cmdaddTodo } from '../../base/acciones/partes.Acciones';
+import { _cmdlimpioAlerta } from '../../base/acciones/alerta.Acciones';
+import { firebaseConnect } from 'react-redux-firebase';
+
+const mapStateToProps = state => ({
+  partez: state.firebase.data.partez,
+  tipo: state.alerta.tipo,
+  mensaje: state.alerta.mensaje
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      _cmdaddTodo,
+      _cmdlimpioAlerta
+    },
+    dispatch
+  );
+
+const enhance = compose(
+  firebaseConnect(['partez']),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+);
+
+export default enhance(Inventario);
