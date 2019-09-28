5a47ac5c654321b65726aa4f74131a7f3187b53b
Vladimir Rodríguez <fizzvr@gmail.com>
2019-09-27 19:28:09 -0500

    Inventyario

---
+++ src/contenedores/InventarioPagina/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+import Inferior from '../../componentes/inferior';
+import Superior from '../../componentes/Zsuperior';
+import Inventario from '../../componentes/Zinventario';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh'
+  },
+  main: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(1)
+  }
+}));
+
+export default function ProgramaPagina() {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Superior />
+      <Container className={classes.main} component='main' maxWidth='md'>
+        <Inventario />
+      </Container>
+      <Inferior />
+    </div>
+  );
+}
--- src/contenedores/Rutas/index.js
+++ src/contenedores/Rutas/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import TestPagina from '../Test';
 import ProgramaPagina from '../ProgramaPagina';
+import InventarioPagina from '../InventarioPagina';
 
 const RutasAssy = () => {
   return (
@@ -9,6 +10,7 @@ const RutasAssy = () => {
       <Router>
         <Switch>
           <Route component={ProgramaPagina} exact={true} path='/' />
+          <Route component={InventarioPagina} exact={true} path='/hhp' />
           <Route component={TestPagina} exact={true} path='/test' />
         </Switch>
       </Router>
