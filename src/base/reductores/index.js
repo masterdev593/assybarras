a543c3cb15a951f3629bb3b1308e32c2b858e8c7
Vladimir Rodríguez <fizzvr@gmail.com>
2019-10-10 15:25:44 -0500

    Setup redux-form

--- src/base/reductores/index.js
+++ src/base/reductores/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase';
+import { reducer as formReducer } from 'redux-form';
 import alertaReductor from './alerta.Reductor';
 import partesReductor from './partes.Reductor';
 
 export default combineReducers({
   firebase: firebaseReducer,
+  form: formReducer,
   alerta: alertaReductor,
   partes: partesReductor
 });
