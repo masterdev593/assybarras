67a4e7977267ee3ac2ebda45a5182a3b0b12f1e6
Vladimir Rodríguez <fizzvr@gmail.com>
2019-10-14 18:40:31 -0500

    Add formik as main form package

--- src/base/reductores/index.js
+++ src/base/reductores/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import { firebaseReducer } from 'react-redux-firebase';
-import { reducer as formReducer } from 'redux-form';
 import alertaReductor from './alerta.Reductor';
 import partesReductor from './partes.Reductor';
 
 export default combineReducers({
   firebase: firebaseReducer,
-  form: formReducer,
   alerta: alertaReductor,
   partes: partesReductor
 });
--- src/componentes/assybarrasForm/asynValidate.js
+++
@@ -1,8 +0,0 @@
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
-
-export default (async function asyncValidate(values) {
-  await sleep(1000);
-  if (['foo@foo.com', 'bar@bar.com'].includes(values.email)) {
-    throw { email: 'Email already Exists' };
-  }
-});
