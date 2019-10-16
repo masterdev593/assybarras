09c3200db4a8c020e512e149a2ccdb7fedb24886
Vladimir Rodríguez <fizzvr@gmail.com>
2019-10-15 19:29:38 -0500

    Formik Updates

--- src/componentes/assybarrasForm/assyBarrasFormMain.js
+++ src/componentes/assybarrasForm/assyBarrasFormMain.js
@@ -1,63 +1,115 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
-import PropTypes from 'prop-types';
-// core
+import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import asyncValidate from './asynValidate';
-import validate from './validate';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import NameIcon from '@material-ui/icons/SupervisorAccount';
+import LockIcon from '@material-ui/icons/Lock';
+import EmailIcon from '@material-ui/icons/Email';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-const renderTextField = (
-  { input, error, helpText, ...custom }
-) => (
-    <TextField
-      error={error}
-      helperText={helpText === '' ? 'Necesario' : ' '}
-      {...input}
-      {...custom}
-    />
-  );
+const styles = theme => ({
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 250
+  }
+});
 
-const MaterialUiForm = props => {
-  const { onSubmit, pristine, reset, submitting } = props;
+const Form = props => {
+  const {
+    values: { name, email, password, confirmPassword, cantidad },
+    errors,
+    touched,
+    handleSubmit,
+    handleChange,
+    isValid,
+    setFieldTouched,
+    classes
+  } = props;
+  console.table(props);
+
+  const change = (name, e) => {
+    e.persist();
+    handleChange(e);
+    setFieldTouched(name, true, false);
+  };
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <Field component={renderTextField} label='First Name' name='parte' />
-      </div>
-      <div>
-        <Field component={renderTextField} label='Last Name' name='descripcion' />
-      </div>
-      <div>
-        <Field component={renderTextField} label='Email' name='email' />
-      </div>
-      <div>
-        <Field
-          component={renderTextField}
-          label='Notes'
-          multiLine={true}
-          name='notes'
-          rows={2}
-        />
-      </div>
-      <div>
-        <button disabled={pristine || submitting} type='submit'>Submit</button>
-        <button disabled={pristine || submitting} onClick={reset} type='button'>
-          Clear Values
-        </button>
-      </div>
+    <form onSubmit={handleSubmit}>
+      <TextField
+        className={classes.textField}
+        error={Boolean(errors.name)}
+        helperText={touched.name ? errors.name : ''}
+        label='Name'
+        margin='normal'
+        name='name'
+        value={name}
+      />
+      <div>{errors.name ? errors.name : ''}</div>
+      <TextField
+        className={classes.textField}
+        error={Boolean(errors.email)}
+        helperText={touched.email ? errors.email : ''}
+        label='Email'
+        margin='normal'
+        name='email'
+        onChange={handleChange}
+        value={email}
+      />
+      <div>{errors.email ? errors.email : ''}</div>
+      <TextField
+        className={classes.textField}
+        error={Boolean(errors.password)}
+        helperText={touched.password ? errors.password : ''}
+        label='Password'
+        margin='normal'
+        name='password'
+        onChange={handleChange}
+        type='password'
+        value={password}
+      />
+      <div>{errors.password}</div>
+      <TextField
+        className={classes.textField}
+        error={Boolean(errors.confirmPassword)}
+        helperText={touched.confirmPassword ? errors.confirmPassword : ''}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position='start'>
+              <LockIcon />
+            </InputAdornment>
+          )
+        }}
+        label='Confirm Password'
+        margin='normal'
+        name='confirmPassword'
+        onChange={handleChange}
+        type='password'
+        value={confirmPassword}
+      />
+      <div>{errors.confirmPassword}</div>
+      <TextField
+        className={classes.textField}
+        label='CANTIDAD'
+        margin='normal'
+        name='cantidad'
+        onChange={handleChange}
+        required={true}
+        value={cantidad}
+      />
+      <div>{errors.cantidad}</div>
+      <Button
+        color='secondary'
+        disabled={!isValid}
+        margin='normal'
+        type='submit'
+        variant='contained'
+        >
+        Submit
+			</Button>
     </form>
   );
 };
 
-MaterialUiForm.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-  helpText: PropTypes.string,
-  error: PropTypes.bool
-};
-
-export default reduxForm({
-  form: 'MaterialUiForm',
-  validate,
-  asyncValidate
-})(MaterialUiForm);
+export default withStyles(styles)(Form);
--- src/componentes/assybarrasForm/index.js
+++ src/componentes/assybarrasForm/index.js
@@ -1,3 +1,68 @@
-import assyBarrasFormMain from './assyBarrasFormMain';
+/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Formik } from 'formik';
+import withStyles from '@material-ui/core/styles/withStyles';
+import Paper from '@material-ui/core/Paper';
+import Form from './assyBarrasFormMain';
+import * as Yup from 'yup';
 
-export default assyBarrasFormMain;
+const styles = theme => ({
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 5}px ${theme.spacing.unit * 5}px ${theme
+      .spacing.unit * 5}px`
+  },
+  container: {
+    maxWidth: '200px'
+  }
+});
+
+const validationSchema = Yup.object({
+  name: Yup.string('Enter a name').required('Name is required'),
+  email: Yup.string('Enter your email')
+    .email('Enter a valid email')
+    .required('Email is required'),
+  password: Yup.string('')
+    .min(8, 'Password must contain atleast 8 characters')
+    .required('Enter your password'),
+  confirmPassword: Yup.string('Enter your password')
+    .required('Confirm your password')
+    .oneOf([Yup.ref('password')], 'Password does not match'),
+  cantidad: Yup.number('Debe ser un número')
+    .required('Cantidad Requerida')
+    .positive('Debe ser mayor a 1')
+});
+
+class InputForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  submit = data => {
+    console.log(data);
+  };
+  render() {
+    const { classes } = this.props;
+    const values = { name: 'GH', email: '', confirmPassword: '', password: '', cantidad: 23 };
+    return (
+      <React.Fragment>
+        <Formik
+          initialValues={values}
+          onSubmit={this.submit}
+          render={props => <Form {...props} />}
+          validationSchema={validationSchema}
+        />
+      </React.Fragment>
+    );
+  }
+}
+
+InputForm.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(InputForm);
