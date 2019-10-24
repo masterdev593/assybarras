50fcf8e01bfef242abb24280dfb0a73dac90abc7
Vladimir Rodríguez <fizzvr@gmail.com>
2019-10-24 18:50:46 -0500

    Constantes  partes

--- src/base/acciones/constantes.js
+++ src/base/acciones/constantes.js
@@ -5,4 +5,7 @@ export const ALERTA_ADVERTENCIA = 'alerta/ALERTA_ADVERTENCIA';
 export const ALERTA_INFO = 'alerta/ALERTA_INFO';
 export const ALERTA_LIMPIAR = 'alerta/ALERTA_LIMPIAR';
 // Partes
-export const FETCH_TODOS = 'partes/FETCH_TODOS';
+export const CAT_PARTES_SOLICITA = 'partes/CAT_PARTES_SOLICITA';
+export const CAT_PARTES_RECIBE = 'partes/CAT_PARTES_RECIBE';
+export const CAT_PARTES_AGREGA = 'partes/CAT_PARTES_AGREGA';
+export const CAT_PARTES_ERROR = 'partes/CAT_PARTES_ERROR';
