fa6bd3f1fa417fe63449ae4120f85be8d8559a8f
Vladimir Rodríguez <fizzvr@gmail.com>
2019-10-25 18:21:46 -0500

    Update reductor

--- src/base/reductores/partes.Reductor.js
+++ src/base/reductores/partes.Reductor.js
@@ -1,9 +1,38 @@
-import { FETCH_TODOS } from '../acciones/constantes';
+import {
+  FORM_ETIQUETAS_PARTE_UPDATE,
+  FORM_ETIQUETAS_DESCRIPCION_UPDATE,
+  FORM_ETIQUETAS_SOLICITA,
+  FORM_ETIQUETAS_ERROR
+} from '../acciones/constantes';
 
-export default (state = {}, action) => {
+const initialState = {
+  estaCargando: false,
+  laparte: '',
+  lades: ''
+};
+
+export default (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_TODOS:
-      return action.payload;
+    case FORM_ETIQUETAS_SOLICITA:
+      return {
+        ...state,
+        estaCargando: true
+      };
+    case FORM_ETIQUETAS_PARTE_UPDATE:
+      return {
+        ...state,
+        laparte: action.payload
+      };
+    case FORM_ETIQUETAS_DESCRIPCION_UPDATE:
+      return {
+        ...state,
+        lades: action.payload
+      };
+    case FORM_ETIQUETAS_ERROR:
+      return {
+        ...state,
+        estaCargando: false
+      };
     default:
       return state;
   }
