ddebfcaff4adb27325da26fd3846b8f16c8de317
Vladimir Rodríguez <fizzvr@gmail.com>
2019-10-25 18:21:04 -0500

    Constantes Update

--- src/base/acciones/constantes.js
+++ src/base/acciones/constantes.js
@@ -4,8 +4,10 @@ export const ALERTA_ERROR = 'alerta/ALERTA_ERROR';
 export const ALERTA_ADVERTENCIA = 'alerta/ALERTA_ADVERTENCIA';
 export const ALERTA_INFO = 'alerta/ALERTA_INFO';
 export const ALERTA_LIMPIAR = 'alerta/ALERTA_LIMPIAR';
-// Partes
-export const CAT_PARTES_SOLICITA = 'partes/CAT_PARTES_SOLICITA';
+// FormEtiquetas
+export const FORM_ETIQUETAS_SOLICITA = 'formEtiquetas/CAT_PARTES_SOLICITA';
 export const CAT_PARTES_RECIBE = 'partes/CAT_PARTES_RECIBE';
-export const CAT_PARTES_AGREGA = 'partes/CAT_PARTES_AGREGA';
-export const CAT_PARTES_ERROR = 'partes/CAT_PARTES_ERROR';
+// export const CAT_PARTES_AGREGA = 'partes/CAT_PARTES_AGREGA';
+export const FORM_ETIQUETAS_ERROR = 'formEtiquetas/FORM_ETIQUETAS_ERROR';
+export const FORM_ETIQUETAS_PARTE_UPDATE = 'formEtiquetas/FORM_ETIQUETAS_PARTE_UPDATE';
+export const FORM_ETIQUETAS_DESCRIPCION_UPDATE = 'formEtiquetas/FORM_ETIQUETAS_DESCRIPCION_UPDATE';
