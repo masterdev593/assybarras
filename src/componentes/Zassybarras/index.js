2c00eab1236581d15555d9f055cc9ac34e201910
Vladimir Rodríguez <fizzvr@gmail.com>
2019-10-25 18:22:12 -0500

    Pass state and functions from redux

--- src/componentes/Zassybarras/index.js
+++ src/componentes/Zassybarras/index.js
@@ -1,18 +1,22 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import AssyBarras from './assybarrasMain';
-import { _cmdaddTodo } from '../../base/acciones/partes.Acciones';
+import { _cmdaddParte, _cmdupdateParte, _cmdupdateDes } from '../../base/acciones/partes.Acciones';
 import { _cmdlimpioAlerta } from '../../base/acciones/alerta.Acciones';
 
 const mapStateToProps = state => ({
   tipo: state.alerta.tipo,
-  mensaje: state.alerta.mensaje
+  mensaje: state.alerta.mensaje,
+  laparte: state.partes.laparte,
+  lades: state.partes.lades
 });
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
-      _cmdaddTodo,
+      _cmdaddParte,
+      _cmdupdateParte,
+      _cmdupdateDes,
       _cmdlimpioAlerta
     },
     dispatch
