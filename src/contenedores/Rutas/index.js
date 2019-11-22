b6a153a8c2d6c35cc5af237c31c80b5126b7a2de
Vladimir Rodríguez <fizzvr@gmail.com>
2019-11-22 17:56:26 -0500

    Add a log page

--- src/contenedores/Rutas/index.js
+++ src/contenedores/Rutas/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import TestPagina from '../Test';
 import ProgramaPagina from '../ProgramaPagina';
 import InventarioPagina from '../InventarioPagina';
+import LogPagina from '../LogPagina';
 
 const RutasAssy = () => {
   return (
@@ -11,6 +12,7 @@ const RutasAssy = () => {
         <Switch>
           <Route component={ProgramaPagina} exact={true} path='/' />
           <Route component={InventarioPagina} exact={true} path='/hhp' />
+          <Route component={LogPagina} exact={true} path='/log' />
           <Route component={TestPagina} exact={true} path='/test' />
         </Switch>
       </Router>
