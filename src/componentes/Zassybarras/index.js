6f68d32f4fc61ea9cd6461ac43ff395a8d93bfed
Vladimir Rodríguez <fizzvr@gmail.com>
2019-11-22 17:54:03 -0500

    Remove functions

--- src/componentes/Zassybarras/index.js
+++ src/componentes/Zassybarras/index.js
@@ -1,22 +1,18 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import AssyBarras from './assybarrasMain';
-import { _cmdaddParte, _cmdupdateParte, _cmdupdateDes } from '../../base/acciones/partes.Acciones';
+import { _cmdaddParte } from '../../base/acciones/partes.Acciones';
 import { _cmdlimpioAlerta } from '../../base/acciones/alerta.Acciones';
 
 const mapStateToProps = state => ({
   tipo: state.alerta.tipo,
-  mensaje: state.alerta.mensaje,
-  laparte: state.partes.laparte,
-  lades: state.partes.lades
+  mensaje: state.alerta.mensaje
 });
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
       _cmdaddParte,
-      _cmdupdateParte,
-      _cmdupdateDes,
       _cmdlimpioAlerta
     },
     dispatch
