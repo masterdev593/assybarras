23634036d82b7558aec3b634319277eb6f71bb19
Vladimir Rodríguez <fizzvr@gmail.com>
2019-11-22 17:52:08 -0500

    Setup axios

--- src/base/configurarBase.dev.js
+++ src/base/configurarBase.dev.js
@@ -1,16 +1,27 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import { getFirebase } from 'react-redux-firebase';
+import axios from 'axios';
+// import { getFirebase } from 'react-redux-firebase';
 import rootReducer from './reductores';
 
 const configurarBase = preloadedState => {
-
-  const middleware = [thunk.withExtraArgument(getFirebase), logger];
-
+  const defaultOptions = {
+    baseURL: 'http://192.168.1.148:5005',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  const axiosInstance = axios.create(defaultOptions);
+  axiosInstance.interceptors.request.use(function(config) {
+    // const token = store.getState().session.token;
+    const token = localStorage.getItem('token');
+    config.headers.Authorization = token ? `${token}` : '';
+    return config;
+  });
   const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const middleware = [thunk.withExtraArgument(axiosInstance), logger];
   const store = createStore(
     rootReducer,
     preloadedState,
--- src/base/configurarBase.prod.js
+++ src/base/configurarBase.prod.js
@@ -1,11 +1,24 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import axios from 'axios';
 import { getFirebase } from 'react-redux-firebase';
 import rootReducer from './reductores';
 
 const configurarBase = preloadedState => {
-
-  const middleware = [thunk.withExtraArgument(getFirebase)];
+  const defaultOptions = {
+    baseURL: 'http://192.168.1.148:5005',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  const axiosInstance = axios.create(defaultOptions);
+  axiosInstance.interceptors.request.use(function(config) {
+    // const token = store.getState().session.token;
+    const token = localStorage.getItem('token');
+    config.headers.Authorization = token ? `${token}` : '';
+    return config;
+  });
+  const middleware = [thunk.withExtraArgument({ getFirebase, axiosInstance })];
 
   const store = createStore(
     rootReducer,
