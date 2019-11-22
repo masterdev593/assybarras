c1534447a57c960cff7921dd3fcd788c2c40de97
Vladimir Rodríguez <fizzvr@gmail.com>
2019-11-22 17:55:11 -0500

    Main inventario

--- src/componentes/Zinventario/inventarioMain.js
+++ src/componentes/Zinventario/inventarioMain.js
@@ -1,82 +1,38 @@
-import React from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { isLoaded, isEmpty } from 'react-redux-firebase/lib/helpers';
 import Loader from '../loader';
 import Alerta from '../alerta';
+import Tabla from '../tabla';
+// import Dialog from '../dialog';
+// import ListaSimpleVista from './ListaSimpleVista';
 
-const StyledTableCell = withStyles(theme => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.secondary.main
-  },
-  body: {
-    fontSize: 14
+class inventarioMain extends Component {
+  constructor(props) {
+    super(props);
   }
-}))(TableCell);
 
-const StyledTableRow = withStyles(theme => ({
-  root: {
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.background.default
-    }
+  componentDidMount() {
+    this.props._cmdgetSeries();
   }
-}))(TableRow);
 
-const useStyles = makeStyles(({
-  root: {
-    width: '100%',
-    overflowX: 'auto'
-  },
-  table: {
-    minWidth: 700
+  render() {
+    const { catIdf, estaCargando, tipo, mensaje } = this.props;
+    return (
+      <div className='seriesMain'>
+        {estaCargando && <Loader />}
+        {!estaCargando && <Alerta mensaje={mensaje} tipo={tipo} />}
+        {!estaCargando && <Tabla tileData={catIdf} />}
+      </div>
+    );
   }
-}));
-
-
-export default function CustomizedTables({ partez }) {
-  const classes = useStyles();
-  if (!isLoaded(partez)) {
-    return <Loader />;
-  }
-  if (isEmpty(partez)) {
-    return <Alerta mensaje='BDD sin registros' tipo='warning' />;
-  }
-  const total = Object.keys(partez);
-  // <Alerta mensaje={total.length + ' partes cargadas'} tipo='info' />
-  return (
-    <Paper className={classes.root}>
-    <Alerta mensaje={total.length + ' partes cargadas'} tipo='info' />
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <StyledTableCell >NÚMERO DE PARTE</StyledTableCell>
-            <StyledTableCell align='right'>DESCRIPCIÓN</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {
-            Object.keys(partez).map((key, id) => (
-              <StyledTableRow id={id} key={key}>
-                <StyledTableCell component='th' scope='row'>
-                  {partez[key].parte}
-                </StyledTableCell>
-                <StyledTableCell align='right'>{partez[key].descripcion}</StyledTableCell>
-              </StyledTableRow>
-            ))
-          }
-        </TableBody>
-      </Table>
-    </Paper>
-  );
 }
 
-CustomizedTables.propTypes = {
-  partez: PropTypes.object
+inventarioMain.propTypes = {
+  _cmdgetSeries: PropTypes.func.isRequired,
+  tipo: PropTypes.string,
+  mensaje: PropTypes.string,
+  catIdf: PropTypes.array,
+  estaCargando: PropTypes.bool
 };
+
+export default inventarioMain;
