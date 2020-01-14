772f6a262207c643c12f10d6593db3981b2878bb
Vladimir Rodríguez <fizzvr@gmail.com>
2020-01-14 17:02:09 -0500

    Add a offset for fixed top

--- src/componentes/Zsuperior/superiorMain.js
+++ src/componentes/Zsuperior/superiorMain.js
@@ -11,6 +11,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 
 const useStyles = makeStyles(theme => ({
+  offset: theme.mixins.toolbar,
   search: {
     position: 'relative',
     borderRadius: theme.shape.borderRadius,
@@ -106,144 +107,147 @@ const SuperiorMain = () => {
   }
 
   return (
-    <AppBar className={classes.border} position='fixed'>
-      <Toolbar style={{ justifyContent: 'space-around' }}>
-        <img alt='IDF' className={classes.logo} src={logo} />
-        <div className={classes.title}>
-          <Button color='secondary' component={Link} to='./'>
-            Etiquetas
+    <React.Fragment>
+      <AppBar className={classes.border} position='fixed'>
+        <Toolbar style={{ justifyContent: 'space-around' }}>
+          <img alt='IDF' className={classes.logo} src={logo} />
+          <div className={classes.title}>
+            <Button color='secondary' component={Link} to='./'>
+              Etiquetas
           </Button>
-          <Button
-            aria-controls='menu-appbar'
-            aria-haspopup='true'
-            aria-label='account of current user'
-            color='secondary'
-            onClick={handleMenuIdf}
-            >
-            IDF
+            <Button
+              aria-controls='menu-appbar'
+              aria-haspopup='true'
+              aria-label='account of current user'
+              color='secondary'
+              onClick={handleMenuIdf}
+              >
+              IDF
           </Button>
-          <Menu
-            anchorEl={anchorEl}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right'
-            }}
-            id='menu-appbar'
-            keepMounted
-            onClose={handleClose}
-            open={open}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right'
-            }}
-            >
-            <MenuItem component={Link} onClick={handleClose} to='./hhp'>
-              Inventario
+            <Menu
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              id='menu-appbar'
+              keepMounted
+              onClose={handleClose}
+              open={open}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              >
+              <MenuItem component={Link} onClick={handleClose} to='./hhp'>
+                Inventario
             </MenuItem>
-            <MenuItem component={Link} onClick={handleClose} to='./log'>
-              Log
+              <MenuItem component={Link} onClick={handleClose} to='./log'>
+                Log
             </MenuItem>
-          </Menu>
-          <Button
-            aria-controls='menu-protection'
-            aria-haspopup='true'
-            aria-label='account of current user'
-            color='secondary'
-            onClick={handleMenuProtectionPro}
-            >
-            Protection Pro
+            </Menu>
+            <Button
+              aria-controls='menu-protection'
+              aria-haspopup='true'
+              aria-label='account of current user'
+              color='secondary'
+              onClick={handleMenuProtectionPro}
+              >
+              Protection Pro
           </Button>
-          <Menu
-            anchorEl={anchorEl2}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'left'
-            }}
-            id='menu-protection'
-            keepMounted
-            onClose={handleClose2}
-            open={open2}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left'
-            }}
-            >
-            <MenuItem component={Link} onClick={handleClose2} to='./pp'>
-              Inventario
+            <Menu
+              anchorEl={anchorEl2}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'left'
+              }}
+              id='menu-protection'
+              keepMounted
+              onClose={handleClose2}
+              open={open2}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left'
+              }}
+              >
+              <MenuItem component={Link} onClick={handleClose2} to='./pp'>
+                Inventario
             </MenuItem>
-          </Menu>
-          <Button
-            aria-controls='menu-protection'
-            aria-haspopup='true'
-            aria-label='account of current user'
-            color='secondary'
-            onClick={handleMenuSony}
-            >
-            sony
+            </Menu>
+            <Button
+              aria-controls='menu-protection'
+              aria-haspopup='true'
+              aria-label='account of current user'
+              color='secondary'
+              onClick={handleMenuSony}
+              >
+              sony
           </Button>
-          <Menu
-            anchorEl={anchorEl3}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'left'
-            }}
-            id='menu-protection'
-            keepMounted
-            onClose={handleClose3}
-            open={open3}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left'
-            }}
-            >
-            <MenuItem component={Link} onClick={handleClose3} to='./sony'>
-              Inventario
+            <Menu
+              anchorEl={anchorEl3}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'left'
+              }}
+              id='menu-protection'
+              keepMounted
+              onClose={handleClose3}
+              open={open3}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left'
+              }}
+              >
+              <MenuItem component={Link} onClick={handleClose3} to='./sony'>
+                Inventario
             </MenuItem>
-          </Menu>
-          <Button
-            aria-controls='menu-protection'
-            aria-haspopup='true'
-            aria-label='account of current user'
-            color='secondary'
-            onClick={handleMenuSkins}
-            >
-            skins
+            </Menu>
+            <Button
+              aria-controls='menu-protection'
+              aria-haspopup='true'
+              aria-label='account of current user'
+              color='secondary'
+              onClick={handleMenuSkins}
+              >
+              skins
           </Button>
-          <Menu
-            anchorEl={anchorEl4}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'left'
-            }}
-            id='menu-protection'
-            keepMounted
-            onClose={handleClose4}
-            open={open4}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left'
-            }}
-            >
-            <MenuItem component={Link} onClick={handleClose4} to='./skins'>
-              Inventario
+            <Menu
+              anchorEl={anchorEl4}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'left'
+              }}
+              id='menu-protection'
+              keepMounted
+              onClose={handleClose4}
+              open={open4}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left'
+              }}
+              >
+              <MenuItem component={Link} onClick={handleClose4} to='./skins'>
+                Inventario
             </MenuItem>
-          </Menu>
-        </div>
-        <div className={classes.search}>
-          <div className={classes.searchIcon}>
-            <SearchIcon />
+            </Menu>
           </div>
-          <InputBase
-            classes={{
-              root: classes.inputRoot,
-              input: classes.inputInput
-            }}
-            inputProps={{ 'aria-label': 'buscar' }}
-            placeholder='Buscar...'
-          />
-        </div>
-      </Toolbar>
-    </AppBar>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+              inputProps={{ 'aria-label': 'buscar' }}
+              placeholder='Buscar...'
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.offset} />
+    </React.Fragment>
   );
 };
 
